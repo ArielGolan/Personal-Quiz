9192824c706b1288a2ab0e65df15b66fc44f0d09
Ariel Golan <ariel.golan.ag@gmail.com>
2022-08-21 11:03:40 +0300

    initial commit

--- Personal Quiz/ViewControllers/ResultViewController.swift
+++ Personal Quiz/ViewControllers/ResultViewController.swift
@@ -10,17 +10,50 @@ import UIKit
 class ResultViewController: UIViewController {
 
     @IBOutlet var animalTypeLable: UILabel!
-    @IBOutlet var descriptionLable: UIView!
+    @IBOutlet var descriptionLable: UILabel!
+    
     
     var answers: [Answer]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        updateResult()
     }
     
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
     }
-    
-
 }
+
+
+// MARK: - Private Methods
+extension ResultViewController {
+    private func updateResult() {
+        var frequencyOfAnimals: [Animal : Int] = [:]
+        let animals = answers.map { $0.animal }
+        
+        for animal in animals {
+            if let animalTypeCount = frequencyOfAnimals[animal] {
+                frequencyOfAnimals.updateValue(animalTypeCount + 1, forKey: animal)
+            } else {
+                frequencyOfAnimals[animal] = 1
+            }
+        }
+        
+        let sortedFrequencyAnimals = frequencyOfAnimals.sorted { $0.value > $1.value }
+        guard let mostfrequensyAnimals = sortedFrequencyAnimals.first?.key else { return }
+        
+        updateUI(with: mostfrequensyAnimals)
+    }
+    
+    private func updateUI(with animal: Animal?) {
+        animalTypeLable.text = "Вы - \(animal?.rawValue ?? "🐶")!"
+        descriptionLable.text = animal?.definition ?? ""
+    }
+}
+
+
+
+
+
